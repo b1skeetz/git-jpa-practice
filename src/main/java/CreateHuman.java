4d8bd558b171394e7ed1109a81877cdf773380d1
b1skeetz <damirk120404@gmail.com>
2024-02-03 13:39:12 +0600

    update is ready

--- src/main/java/CreateHuman.java
+++ src/main/java/CreateHuman.java
@@ -12,15 +12,83 @@ public class CreateHuman {
 
         System.out.print("Введите имя человека: ");
         String searchName = scanner.nextLine();
+        String answer;
         try {
             TypedQuery<Human> searchForHuman = manager.createQuery("select h from Human h where h.name = ?1", Human.class);
             searchForHuman.setParameter(1, searchName);
             Human foundHuman = searchForHuman.getSingleResult();
-            System.out.println(foundHuman);
+            System.out.println("Пользователь найден!\nИзменить запись? [y/n]");
+            answer = scanner.nextLine();
+            switch (answer) {
+                case "y", "yes" -> System.out.println("Изменение пользователя:");
+                case "n", "no" -> {
+                    System.out.println("Изменение отклонено");
+                    System.out.println(foundHuman);
+                    return;
+                }
+                default -> {
+                    System.out.println("Неверный ответ!");
+                    System.out.println("Изменение отклонено");
+                    return;
+                }
+            }
+
+            System.out.println("Выберите новый город: ");
+            TypedQuery<City> selectAllCitiesUpdate = manager.createQuery("select c from City c", City.class);
+            List<City> citiesList = selectAllCitiesUpdate.getResultList();
+            for (City city : citiesList) {
+                System.out.println(city.getId() + ") " + city.getName() + " (" + city.getCountry() + ").");
+            }
+            String isIdNull = scanner.nextLine();
+            Long cityId = 0L;
+            City chosenCity;
+            if(isIdNull == null){
+                chosenCity = foundHuman.getCity();
+            } else {
+                cityId = Long.parseLong(isIdNull);
+                chosenCity = citiesList.get((int) (cityId - 1));
+            }
+
+            System.out.print("Введите новое имя: ");
+            String name = searchName;
+            if(name == null){
+                name = foundHuman.getName();
+            }
+
+            System.out.print("Введите новый возраст: ");
+            String age = scanner.nextLine();
+            Integer updateAge;
+            if(age == null){
+                updateAge = foundHuman.getAge();
+            } else {
+                updateAge = Integer.parseInt(age);
+            }
+
+            System.out.print("Введите адрес: ");
+            String address = scanner.nextLine();
+            if(address == null){
+                address = foundHuman.getAddress();
+            }
+            try{
+                manager.getTransaction().begin();
+                Query updateHuman = manager.createQuery("update Human h set h.name = ?1, h.age = ?2, h.address = ?3, h.city = ?4" +
+                        "where h.id = ?5");
+                updateHuman.setParameter(1, name);
+                updateHuman.setParameter(2, updateAge);
+                updateHuman.setParameter(3, address);
+                updateHuman.setParameter(4, chosenCity);
+                updateHuman.setParameter(5, foundHuman.getId());
+                updateHuman.executeUpdate();
+                manager.getTransaction().commit();
+            } catch (RuntimeException e){
+                System.out.println(e.getMessage());
+                manager.getTransaction().rollback();
+                throw new RuntimeException();
+            }
             return;
         } catch (NoResultException e){
             System.out.println("Пользователь с таким именем не найден!\nСоздать запись нового человека? [y/n]");
-            String answer = scanner.nextLine();
+            answer = scanner.nextLine();
             switch (answer) {
                 case "y", "yes" -> System.out.println("Создание пользователя:");
                 case "n", "no" -> {
@@ -34,7 +102,6 @@ public class CreateHuman {
                 }
             }
         }
-
 
         System.out.println("Выберите город: ");
         TypedQuery<City> selectAllCities = manager.createQuery("select c from City c", City.class);
